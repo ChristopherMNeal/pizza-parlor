9ab201d892c03b091b90172c9a26355dd02812fc
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-05 10:04:10 -0700

    Change business logic to accept size in string format 'm/l/xl num inch' instead of number to simplify displayPizza function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 // Business logic for Pizza()
-function Pizza(toppingsArray, sizeInInches) {
+function Pizza(toppingsArray, sizeString) {
   this.toppings = toppingsArray;
-  this.size = sizeInInches;
+  this.size = sizeString;
 }
 
 Pizza.prototype.calculatePrice = function() {
   let cost = 0;
   switch (this.size){
-    case 10:
+    case "Medium 10 inch":
       cost = 11;
       cost += 1 * this.toppings.length;
       break;
-    case 14:
+    case "Large 14 inch":
       cost = 17;
       cost += 2 * this.toppings.length;
       break;
-    case 18:
+    case "XL 18 inch":
       cost = 23;
       cost += 3 * this.toppings.length;
       break;
@@ -23,12 +23,10 @@ Pizza.prototype.calculatePrice = function() {
   return cost;
 }
 
-
-
 Pizza.prototype.displayPizza = function() {
   cost = this.calculatePrice();
   return "Your Pizza:<br>" + this.size + "<br>Toppings:<br>" +  this.toppings + "<br>$" + this.calculatePrice();
 }
-const pizzaLarge = new Pizza (["peppers", "pepperoni", "pepperoncini"], 14);
-const pizzaMed = new Pizza (["pepper", "peppers", "pepperoni", "pepperoncini"], 10);
-const pizzaXl = new Pizza (["pepper", "peppers", "pepperoni", "pepperoncini", "roasted red peppers"], 18);
+const pizzaLarge = new Pizza (["peppers", "pepperoni", "pepperoncini"], "Large 14 inch");
+const pizzaMed = new Pizza (["pepper", "peppers", "pepperoni", "pepperoncini"], "Medium 10 inch");
+const pizzaXl = new Pizza (["pepper", "peppers", "pepperoni", "pepperoncini", "roasted red peppers"], "XL 18 inch");

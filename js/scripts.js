acb5d145beedafcb08a37059d43570e2adedd895
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-05 09:17:34 -0700

    Pass first test, Pizza.prototype.calculatePrice() returns 17

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,21 @@ function Pizza(toppingsArray, sizeInInches) {
   this.toppings = toppingsArray;
   this.size = sizeInInches;
 }
+
+Pizza.prototype.calculatePrice = function() {
+  let cost = 0;
+  switch (this.size){
+    case 10:
+      cost = 11;
+      break;
+    case 14:
+      cost = 17;
+      break;
+    case 18:
+      cost = 23;
+      break;
+  }
+  return cost;
+}
+
 const pizzaOne = new Pizza (["peppers", "pepperoni", "pepperoncini"], 14);
